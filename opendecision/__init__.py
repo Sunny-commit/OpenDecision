acb0615781fff0a6a9943edc57f59b1ffefca9e0
pateti Chandu <sunny6866666@gmail.com>
2026-09-25 13:48:09 +0530

    Export tool guard helpers

--- opendecision/__init__.py
+++ opendecision/__init__.py
@@ -3,13 +3,17 @@
 from .guard import DecisionGuard
 from .models import DecisionQuestion, DecisionResult
 from .policies import DecisionPolicy, load_policy
+from .tool_guard import ToolBlockedError, ToolReviewRequiredError, protect_tool
 
 __all__ = [
     "DecisionGuard",
     "DecisionQuestion",
     "DecisionResult",
     "DecisionPolicy",
+    "ToolBlockedError",
+    "ToolReviewRequiredError",
     "load_policy",
+    "protect_tool",
 ]
 
 __version__ = "0.1.0"
